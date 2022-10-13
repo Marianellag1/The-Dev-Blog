86f3607c6af449d7ca30fc6650ecbf01eb86d079
Mgamboa <marianellag400@gmail.com>
2022-10-12 20:06:53 -0500

    finished homeRoutes, also started heroku, page doesn't show challenge, shows something else, will have to push heroku to see if actual page shows up.

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -3,9 +3,43 @@ const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 //get all post
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [User],
+        });
+        const posts = postData.map((post) =>
+            post.get({ plain: true })
+        );
+        res.render('all-posts', { posts, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        res.redirect('login')
+    }
+});
+
 
 //get single post
+router.get('/post/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk({
+            where: {
+                id: req.session.id
+            },
+            include: [
 
+                User, {
+                    model: Comment,
+                    include: [User]
+                },
+            ]
+        });
+        const posts = postData.map((post) =>
+            post.get({ plain: true }));
+        res.render('single-post', { posts, loggedIn: req.session.loggedIn })
+    } catch (err) {
+        res.redirect('login')
+    }
+})
 
 //login route
 router.get('/login', (req, res) => {
