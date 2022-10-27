d9839eb66207fb5a273076dfcd561354d427c8f8
Mgamboa <marianellag400@gmail.com>
2022-10-27 17:52:06 -0500

    removed extra lines, final push

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -72,15 +72,4 @@ router.get('/new', withAuth, (req, res) => {
 // })
 
 module.exports = router;
-// WHEN I click on the dashboard option in the navigation
-// THEN I am taken to the dashboard and presented with any blog posts I have already created and the option to add a new blog post
-// WHEN I click on the button to add a new blog post
-// THEN I am prompted to enter both a title and contents for my blog post
-// WHEN I click on the button to create a new blog post
-// THEN the title and contents of my post are saved and I am taken back to an updated dashboard with my new blog post
-// WHEN I click on one of my existing posts in the dashboard
-// THEN I am able to delete or update my post and taken back to an updated dashboard
-
-//check if this works here>>>>>>>>>
-// get single post
 
